561a4166217867d7c079534630c1077545f0a4a2
unknown <1056628965@qq.com>
2022-04-15 23:12:42 +0800

    feat: mirror&download&init

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,12 @@
 const program = require('commander')
 // 请求 lib/update.js
 const updateChk = require('../lib/update')
+// 请求 lib/mirror.js
+const setMirror = require('../lib/mirror')
+// 请求 lib/download.js
+const dlTemplate = require('../lib/download')
+// 请求 lib/init.js
+const initProject = require('../lib/init')
 
 // 从 package.json 文件中请求 version 字段的值，-v和--version是参数
 program.version(require('../package.json').version, '-v, --version')
@@ -18,6 +24,32 @@ program
     // 执行 lib/update.js 里面的操作
     updateChk()
   })
+
+// mirror 切换镜像链接
+program
+.command('mirror <template_mirror>')
+.description("Set the template mirror.")
+.action((tplMirror) => {
+  setMirror(tplMirror)
+})
+
+// template 下载/更新模板
+program
+  .command('template')
+  .description("Download template from mirror.")
+  .action(() => {
+    dlTemplate()
+  })
+
+// init 初始化项目
+program
+.name('js-plugin-cli')
+.usage('<commands> [options]')
+.command('init <project_name>')
+.description('Create a javascript plugin project.')
+.action(project => {
+  initProject(project)
+})
 
 // 解析命令行参数
 program.parse(process.argv)
